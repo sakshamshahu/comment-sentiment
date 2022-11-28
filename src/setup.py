65f43b7e2627a5087b1995d76fbaa703032fca2b
Saksham-Shahu <sakshamshahu.work@gmail.com>
2022-11-28 16:20:34 +0530

    initial commit

---
+++ src/setup.py
@@ -0,0 +1,55 @@
+import pandas as pd 
+import numpy as np
+
+comments_df = pd.read_csv("/home/saksham/AVI/Projects/Sentiment-Comments/data/raw/Comments.csv",encoding='cp1252')
+positive_df = pd.read_csv("data/raw/positive-words.csv")
+negative_df = pd.read_csv("data/raw/negative-words.csv",encoding='cp1252')
+commentss = comments_df["Comments"].to_numpy()
+
+# splited = np.char.split(commentss[1])
+n = commentss.size
+positive_arr =positive_df["Positive Words"].to_numpy()
+negative_arr = negative_df["Negative Words"].to_numpy()
+
+# print(splited)
+# size_splited = splited.size
+# print(size_splited)
+positive_count_list = []
+
+for i in range (0,n):
+    positive_count =0
+    lower_case = np.char.lower(commentss[i])
+    splited = np.char.split(lower_case)
+    split_list = splited.tolist()
+    list_length = len(split_list)
+    for j in range (0,list_length):
+        # positive_count =0
+        for k in range (0,positive_arr.size):
+            if split_list[j] == positive_arr[k]:
+                positive_count += 1
+    positive_count_list.insert(i,positive_count)
+
+print(positive_count_list)
+print(len(positive_count_list))
+
+negative_count_list =[]
+
+
+for i in range (0,n):
+    negative_count =0
+    lower_case = np.char.lower(commentss[i])
+    splited = np.char.split(lower_case)
+    split_list = splited.tolist()
+    list_length = len(split_list)
+    for j in range (0,list_length):
+        for k in range (0,negative_arr.size):
+            if split_list[j] == negative_arr[k]:
+                negative_count += 1
+    negative_count_list.insert(i,negative_count)
+
+print(negative_count_list)
+print(len(negative_count_list))
+
+comments_df["positive"] =np.array(positive_count_list)
+comments_df["negative"] =np.array(negative_count_list)
+print(comments_df)
