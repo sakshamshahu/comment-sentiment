4260611565b8aab718fcb6315f6241e5523af3f2
Saksham-Shahu <sakshamshahu.work@gmail.com>
2022-12-03 22:30:27 +0530

    fix: minor changes

--- src/setup.py
+++ src/setup.py
@@ -32,27 +32,27 @@ for i in range (0,2):
 print(positive_count_list)
 print(len(positive_count_list))
 
-# negative_count_list =[]
+negative_count_list =[]
 
 
-# for i in range (0,n):
-#     negative_count =0
-#     lower_case = np.char.lower(commentss[i])
-#     splited = np.char.split(lower_case)
-#     split_list = splited.tolist()
-#     list_length = len(split_list)
-#     for j in range (0,list_length):
-#         for k in range (0,negative_arr.size):
-#             if split_list[j] == negative_arr[k]:
-#                 negative_count += 1
-#     negative_count_list.insert(i,negative_count)
+for i in range (0,n):
+    negative_count =0
+    lower_case = np.char.lower(commentss[i])
+    splited = np.char.split(lower_case)
+    split_list = splited.tolist()
+    list_length = len(split_list)
+    for j in range (0,list_length):
+        for k in range (0,negative_arr.size):
+            if split_list[j] == negative_arr[k]:
+                negative_count += 1
+    negative_count_list.insert(i,negative_count)
 
-# print(negative_count_list)
-# print(len(negative_count_list))
+print(negative_count_list)
+print(len(negative_count_list))
 
-# comments_df["positive"] =np.array(positive_count_list)
-# comments_df["negative"] =np.array(negative_count_list)
-# print(comments_df)
+comments_df["positive"] =np.array(positive_count_list)
+comments_df["negative"] =np.array(negative_count_list)
+print(comments_df)
 
 # comments_df.to_csv("/home/saksham/AVI/Projects/Sentiment-Comments/data/raw/UpdatedComments.csv")
 # comments_df.to_excel("/home/saksham/AVI/Projects/Sentiment-Comments/data/raw/UpdatedComments.xlsx", sheet_name="Testing", index=False)
